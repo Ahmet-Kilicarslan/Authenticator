b0d0d63f63493cb59f0b8cb30206e8f469aa1013
Ahmet-kilicarslan <Ahmet.kilicarslan1717@gamil.com>
2025-11-27 11:55:44 +0300

    commit after styling login and register components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import registerComponent from './components/register-component/register-component'
+import otpComponent from './components/otp-component/otp-component'
+import loginComponent from './components/login-component/login-component'
 
 export const routes: Routes = [
 
   //=============Routes==================
 
+  {path: 'Login', component: loginComponent},
+  {path: 'Register', component: registerComponent},
+  {path: 'Otp', component: otpComponent},
 
+  {path: '**', redirectTo: '/Login'}
 ];
